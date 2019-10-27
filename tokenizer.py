b6baff1022c6cdedd737739c5ba47dc62655b536
ahmed.nasr9677@gmail.com <ahmed.nasr9677@gmail.com>
2019-10-27 20:12:07 +0200

    add is_identifier function

--- tokenizer.py
+++ tokenizer.py
@@ -9,7 +9,7 @@ class Tokenizer():
     def __init__(self, input):
         ## private attributes
         self.__input = input
-        self.__digit_regex = "[0-9]+"
+        self.__digit_regex = "[0-9]+" # to do signed 
         self.__identif_regex = "[a-zA-Z]+([a-zA-Z]|[0-9])*|([a-zA-Z]|[0-9])*"
         self.__exp_regex = "" # to do 
         self.__symbols_list = [] # to do 
@@ -22,7 +22,12 @@ class Tokenizer():
         pass
 
     def is_id(self, word):
-        pass
+        match_object = re.match(self.__identif_regex)
+        start, length = match_object.span()
+        if start == 0 and length == len(word):
+            return True
+        else:
+            return False
 
     def is_digit(self, word):
         pass
