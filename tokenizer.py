53ebd152bc0d1a92a76c9d8711c6960b52a5597c
ℓåмєєs ♥ <lameesemad1996@hotmail.com>
2019-10-28 02:53:21 +0200

    implement is_keyword and check_word functions

--- tokenizer.py
+++ tokenizer.py
@@ -1,30 +1,24 @@
-import re # regex lib
+import re  # regex lib
 
 
-
-
-class Tokenizer():
-
+class Tokenizer:
 
     def __init__(self, input):
-        ## private attributes
+        # private attributes
 
         self.__input = input
-        self.__digit_regex = "[0-9]+" # to do signed 
+        self.__digit_regex = "[0-9]+"  # to do signed
         self.__identif_regex = "[a-zA-Z]+([a-zA-Z]|[0-9])*|([a-zA-Z]|[0-9])*"
-        #self.__exp_regex = self.__identif_regex ------>> not working
+        # self.__exp_regex = self.__identif_regex ------>> not working
         self.__symbols_list = ['+', '-', '*', '/', '=', '<', '>', '(', ')', ';', ':='] 
-
 
     def split_to_lines(self, input):
         lines = input.split("\n")
         return lines
 
-    def split_on_spaces(self, lines): #lines is a list
+    def split_on_spaces(self, lines):  # lines is a list
         words = lines.split(" ")
         return words
-
-
 
     def is_id(self, word):
         match_object = re.match(self.__identif_regex)
@@ -34,24 +28,27 @@ class Tokenizer():
         else:
             return False
 
-
     def is_digit(self, word):
-        match_object = re.match(self.__digit_regex )
+        match_object = re.match(self.__digit_regex)
         begin ,  length = match_object.span()
         if begin == 0 and length == len(word):
             return True
         else:
             return False
 
-
+#  WRITE READ IF ELSE RETURN BEGIN END MAIN STRING INT REAL
     def is_keyword(self, word):
-        pass
+        if word == "write" or word == "read" or word == "if" or word == "else" or word == "return" or word == "begin" or word == "end" or word == "main" or word == "string" or word == "int":
+            return True
+        else:
+            return False
 
-    def is_exp(self, word): # to be postponed (state machine, regex is not working)
+    def is_exp(self, word):  # to be postponed (state machine, regex is not working)
         pass
 
     def check_word(self, word):
-        pass
+        if (not(self.is_keyword())) and (not(self.is_digit())) and (not(self.is_exp())) and (not(self.is_id())) and (not(word in self.__symbols_list)):
+            return False
 
     def tokenize_exp(self, exp): # to be postponed 
         pass
