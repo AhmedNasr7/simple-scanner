a770f3ca9c1a39e7656f47361623033e22c04d65
ahmed.nasr9677@gmail.com <ahmed.nasr9677@gmail.com>
2019-10-28 12:34:00 +0200

    add Token class

---
+++ token.py
@@ -0,0 +1,22 @@
+
+
+
+class Token():
+    
+    def __init__(self, value, type):
+        self.__value = value
+        self.__type = type
+
+
+    def value(self):
+        return self.__value
+
+    def type(self):
+        return self.__type
+
+    
+    
+
+
+
+
